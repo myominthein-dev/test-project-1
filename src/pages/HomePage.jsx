d24b82a62289eb3ac80ea36e427710d4b767000b
Zarmani <dthar06@gmail.com>
2024-11-05 20:06:30 +0700

    testimonial section done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import HeaderBlock from "../components/HeaderBlock";
 import MasterfulDesign from "../components/MasterfulDesign";
 import NextLevelInnovation from "../components/NextLevelInnovation";
 import MagicUnleash from "../components/MagicUnleash";
+import Testimonial from "../components/Testimonial";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,7 @@ const HomePage = () => {
       <MasterfulDesign />
       <NextLevelInnovation />
       <MagicUnleash />
+      <Testimonial />
     </div>
   );
 };
