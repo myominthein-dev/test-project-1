e245fe63c35d994172520d387ec740fee6d5b5b1
Zarmani <dthar06@gmail.com>
2024-11-05 14:05:44 +0700

    test

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,14 @@
 const Container = ({ children, className }) => {
   return (
+<<<<<<< Updated upstream
     <section className={`w-full  mx-auto ${className}`}>{children}</section>
+=======
+    <section
+      className={`w-full md:w-[720px]  lg:w-[1000px] mx-auto ${className}`}
+    >
+      {children}
+    </section>
+>>>>>>> Stashed changes
   );
 };
 export default Container;
