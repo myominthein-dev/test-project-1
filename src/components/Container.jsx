44399cbbe0129e4694f24585d2fbfe93b3477d3b
Zarmani <dthar06@gmail.com>
2024-11-05 13:41:03 +0700

    fonts family changed

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,6 @@
 const Container = ({ children, className }) => {
   return (
-    <section
-      className={`w-full md:w-[720px]  lg:w-[1000px] mx-auto ${className}`}
-    >
-      {children}
-    </section>
+    <section className={`w-full  mx-auto ${className}`}>{children}</section>
   );
 };
 export default Container;
