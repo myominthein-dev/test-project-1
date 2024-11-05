e6a6918ec8c13c10323c5ee1cb92f65b62b7b7c6
Zarmani <dthar06@gmail.com>
2024-11-05 13:41:03 +0700

    fonts family changed

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,22 +1,8 @@
 const Container = ({ children, className }) => {
   return (
-<<<<<<< HEAD
-    <section
-      className={`w-full md:w-[720px]  lg:w-[1000px] mx-auto ${className}`}
-    >
+    <section className={`w-full  mx-auto max-w-[1632px] ${className}`}>
       {children}
     </section>
-=======
-<<<<<<< Updated upstream
-    <section className={`w-full  mx-auto ${className}`}>{children}</section>
-=======
-    <section
-      className={`w-full  mx-auto max-w-[1632px] ${className}`}
-    >
-      {children}
-    </section>
->>>>>>> Stashed changes
->>>>>>> main
   );
 };
 export default Container;
