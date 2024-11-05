77cce4cdb1eb2359f9b85dc3241dae14b6a33d21
Zarmani <dthar06@gmail.com>
2024-11-05 14:35:06 +0700

    done: headerbolck responsive

--- src/components/HeaderBlock.jsx
+++ src/components/HeaderBlock.jsx
@@ -9,13 +9,13 @@ const HeaderBlock = ({
   secondHeadline,
 }) => {
   return (
-    <div className="mx-auto text-center">
+    <div className="mx-auto md:text-center px-3">
       <Badge label={label} />
-      <h2 className="text-[50px] font-futura font-bold">
+      <h2 className="text-xl md:text-2xl lg:text-[50px] font-futura font-bold my-3 lg:my-5">
         <span className={`text-${firstColor}-PRIMARY`}>{firstHeadline}</span>{" "}
         <span className={`text-${secondColor}-PRIMARY`}>{secondHeadline}</span>
       </h2>
-      <p className="font-lato leading-6 text-secondaryGrey w-1/3 mx-auto">
+      <p className="text-sm md:text-base font-lato leading-6 text-secondaryGrey w-full md:w-2/3 lg:w-1/3 mx-auto">
         {children}
       </p>
     </div>
