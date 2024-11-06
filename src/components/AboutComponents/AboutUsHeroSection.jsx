994ad03ebba35a42a7865cad2233b65ea2526978
blankverse123000 <blankverse123000@gmail.com>
2024-11-07 01:26:09 +0800

    done_Responsive_hero_&_Transforming_components

--- src/components/AboutComponents/AboutUsHeroSection.jsx
+++ src/components/AboutComponents/AboutUsHeroSection.jsx
@@ -4,24 +4,28 @@ import Container from "../Container";
 
 const AboutUsHeroSection = () => {
   return (
-    <section className=" bg-custom-gradient min-h-screen py-[80px]">
-      <Container className={"py-5 px-5"}>
+    <section className=" bg-custom-gradient  py-[80px]">
+      <Container className={"py-5 px-5  "}>
         <div className=" flex justify-between flex-col items-center gap-10">
-          <div className=" text-center flex  lg:w-[864px] gap-2.5 mx-auto flex-col justify-center items-center  ">
-            <h1 className=" text-blue-PRIMARY lg:text-[22px] font-medium  font-futura">
+          <div className=" flex  lg:w-[864px] md:w-[764px] gap-2.5 mx-auto flex-col justify-center md:items-center md:text-center text-left  ">
+            <h1 className=" text-blue-PRIMARY lg:text-[22px] md:text-[16px] font-medium  font-futura ">
               About us
             </h1>
-            <p className=" lg:w-3/4  font-bold font-futura text-6xl ">
+            <p className=" lg:w-3/4 md:text-[30px] text-[25px] md:w-[435px] font-bold font-futura lg:text-6xl ">
               We are changing the whole{" "}
               <span className="text-blue-PRIMARY ">new design</span>
             </p>
-            <p className=" font-lato text-[#7F7D7D] w-1/2 text-center">
+            <p className=" font-lato text-[#7F7D7D] lg:w-[521px]  md:w-[414px] lg:text-[20px] md:text-[16px] text-[14px] ">
               The only corporate card and spent management platform designed to
               help you spend less.
             </p>
           </div>
           <div className=" flex flex-col items-center justify-center mt-8 ">
-            <img className=" lg:w-[592px]" src={fourPeopleAtTable} alt="" />
+            <img
+              className=" lg:w-[592px] md:w-[422.69px]"
+              src={fourPeopleAtTable}
+              alt=""
+            />
           </div>
         </div>
       </Container>
--- src/components/AboutComponents/TransformBusiness.jsx
+++ src/components/AboutComponents/TransformBusiness.jsx
@@ -5,32 +5,35 @@ import Button from "./Button";
 
 const TransformBusiness = () => {
   return (
-    <section className=" py-[100px]">
+    <section className="">
       <Container className={"py-5 px-5 flex justify-center"}>
-        <div className=" flex  justify-between items-center w-[1220px]">
-          <div className=" flex flex-col   ">
-            <div className="flex flex-col gap-2.5 w-[491px] ">
-              <h1 className=" font-futura font-bold text-2xl w-[363px] md:text-[25px] lg:text-[31px]  my-1">
-                Transforming <br className="" /> business since 2014
-              </h1>
-              <div
-                className=" w-[491px] flex flex-col gap-2.5 text-[#7F7D7D]
+        <div className=" flex md:flex-row flex-col gap-[10px] lg:w-[1220px] md:w-[764px] w-[352px] md:gap-[20px]  justify-between  text-left items-center ">
+          <div className="flex flex-col  gap-2.5 lg:w-[491px] md:w-[372px] w-[352px] ">
+            <h1 className=" font-futura font-bold text-2xl   md:text-[22px] text-[20px] lg:text-[31px]  my-1">
+              Transforming <br className="" /> business since 2014
+            </h1>
+            <div
+              className="  flex flex-col gap-2.5 text-[#7F7D7D]
 "
-              >
-                <p className=" w-[391px] ">
-                  The passion with which we work allows us to transform a
-                  problem into a challenge!
-                </p>
-                <p className=" w-[391px] ">
-                  Technology has made our lives easier in so many ways, which
-                  include a more connivent.
-                </p>
-              </div>
-              <Button className={"mt-5 items-end"} label={"Learn More"} />
+            >
+              <p className=" lg:w-[391px]  w-[334px] md:text-[16px] text-[14px]   ">
+                The passion with which we work allows us to transform a problem
+                into a challenge!
+              </p>
+              <p className=" lg:w-[391px] md:w-[371px] w-[334px] md:text-[16px] text-[14px] ">
+                Technology has made our lives easier in so many ways, which
+                include a more connivent.
+              </p>
             </div>
+            <Button className={"mt-5 items-end"} label={"Learn More"} />
           </div>
-          <div className=" flex items-center justify-center w-[595px]">
-            <img src={desktopComputer} alt="" />
+
+          <div className=" flex items-center justify-center md:mt-0 mt-5 ">
+            <img
+              className="lg:w-[531px] md:w-[372px] w-[352px]"
+              src={desktopComputer}
+              alt=""
+            />
           </div>
         </div>
       </Container>
