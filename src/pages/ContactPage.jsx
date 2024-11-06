e943309525a2dab3efdd29912d5c2a59e4dba7ba
Vee-Scored <myomin9916@gmail.com>
2024-11-07 01:28:15 +0630

    aos error handled

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,11 +4,12 @@ import {
   ContactUsHeroSection,
   ContactUsOffice,
 } from "../components/ContactComponents";
-import AOS from "aos";
+//import AOS from "aos";
 import "aos/dist/aos.css";
+import Aos from "aos";
 const ContactPage = () => {
   useEffect(() => {
-    AOS.init({});
+    Aos.init({});
   }, []);
   return (
     <section className="mx-auto w-[1440px] ">
