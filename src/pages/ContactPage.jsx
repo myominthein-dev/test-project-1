14d1b69b7867b3075b0a5a896e94609dfadef6d8
Vee-Scored <myomin9916@gmail.com>
2024-11-07 11:20:13 +0630

    aos issues solve 2

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,16 +1,17 @@
+'use client'
 import React, { useEffect } from "react";
 import {
   ContactUsAddress,
   ContactUsHeroSection,
   ContactUsOffice,
 } from "../components/ContactComponents";
-//import AOS from "aos";
-//import "aos/dist/aos.css";
-import "../../node_modules/aos/dist/aos.css"
-import Aos from "aos";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
 const ContactPage = () => {
   useEffect(() => {
-    Aos.init({});
+    AOS.init();
   }, []);
   return (
     <section className="mx-auto w-[1440px] ">
