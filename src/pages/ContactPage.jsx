5f14e658d94a47c40404362b61b98d018293456a
Vee-Scored <myomin9916@gmail.com>
2024-11-07 17:35:19 +0630

    reinstall aos@next

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,20 +1,17 @@
-'use client'
+"use client";
 import React, { useEffect } from "react";
 import {
   ContactUsAddress,
   ContactUsHeroSection,
   ContactUsOffice,
 } from "../components/ContactComponents";
-import "aos/dist/aos.css";
 
-
-
+import AOS from "aos"
+import 'aos/dist/aos.css';
 
 const ContactPage = () => {
   useEffect(() => {
-    import('aos').then((AOS) => {
-      AOS.init()
-    })
+    AOS.init()
   }, []);
   return (
     <section className="mx-auto w-[1440px] ">
