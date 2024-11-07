c37e3e35ff912c7035f33ad020e0577a1f16d21f
Vee-Scored <myomin9916@gmail.com>
2024-11-08 01:47:07 +0630

    vite conf update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['aos','framer-motion','@jdion/tilt-react']
+      external: ['aos', 'framer-motion', '@jdion/tilt-react']
     }
   },
   optimizeDeps: {
-    include: ["framer-motion","aos","@jdion/tilt-react"]
+    include: ['framer-motion', 'aos', '@jdion/tilt-react']
+  },
+  resolve: {
+    alias: {
+      'framer-motion': 'node_modules/framer-motion/dist/framer-motion.esm.js',
+      'aos': 'node_modules/aos/dist/aos.js',
+      '@jdion/tilt-react': 'node_modules/@jdion/tilt-react/dist/index.esm.js'
+    }
   }
-})
+});
