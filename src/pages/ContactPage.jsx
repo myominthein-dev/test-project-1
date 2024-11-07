70d1a4f4f4a020bd6409d065aa46a36fe50833b4
ArrKuu77 <aungkungsan@gmail.com>
2024-11-07 05:10:09 -1100

    cancleAOS

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -6,13 +6,13 @@ import {
   ContactUsOffice,
 } from "../components/ContactComponents";
 
-import AOS from "aos";
-import "aos/dist/aos.css";
+// import AOS from "aos"
+// import 'aos/dist/aos.css';
 
 const ContactPage = () => {
-  useEffect(() => {
-    AOS.init({});
-  }, []);
+  // useEffect(() => {
+  //   AOS.init({})
+  // }, []);
   return (
     <section className="mx-auto w-[1440px] ">
       <ContactUsHeroSection />
