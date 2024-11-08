a815f243aed8ca1a69178376a9e32944bb0f0f5e
blankverse123000 <blankverse123000@gmail.com>
2024-11-08 15:12:36 +0800

    Added_About_Founder_Component

---
+++ src/components/AboutComponents/AboutFounder.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Container from "../Container";
+import manWithCoffee from "../../assets/AboutAssets/man-with-coffee-at-work.png";
+const AboutFounder = () => {
+  return (
+    <section>
+      <Container className={"py-5 px-5 flex justify-center"}>
+        <div className=" lg:w-[1220px] bg-lightBlue rounded-[20px] flex  justify-between p-10 shadow-inner md:items-center md:flex-row flex-col-reverse  ">
+          <div className=" lg:w-[526px] lg:h-[195px] md:w-[363px] w-[352px] flex-col justify-between flex md:mt-0 mt-10">
+            <h1 className="font-futura font-bold text-2xl   md:text-[22px] text-[20px] lg:text-[31px] md:inline-block  hidden my-1 leading-[41.2px] tracking-wider">
+              About the founder
+            </h1>
+            <div className=" flex flex-col lg:w[526px] gap-[20px] h-[114px]">
+              <p className=" font-lato lg:w-[363px]  w-[334px] md:text-[16px] text-[14px] font-[500px] text-secondaryGrey leading-[28px] tracking-tight">
+                Mr.Peter, Founder of techno skill
+              </p>
+              <p className=" font-futura font-[500px] lg:text-[25px] text-[20px]  text-secondaryBlue leading-[33.2px] tracking-[-0.1%]">{`“Join      us for an exciting journey towards digital 
+success”`}</p>
+            </div>
+          </div>
+          <div className="  flex  justify-end">
+            <img
+              className=" md:w-[418px] w-[352px]  "
+              src={manWithCoffee}
+              alt=""
+            />
+          </div>
+          <h1 className="font-futura font-bold text-2xl  text-[24px] md:hidden   my-1 leading-[41.2px] tracking-wider">
+            About the founder
+          </h1>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutFounder;
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,14 +3,18 @@ import Container from "../components/Container";
 import AboutUsHeroSection from "../components/AboutComponents/AboutUsHeroSection";
 import TransformBusiness from "../components/AboutComponents/TransformBusiness";
 import Ambition from "../components/AboutComponents/Ambition";
+import AboutFounder from "../components/AboutComponents/AboutFounder";
 
 const AboutPage = () => {
   return (
     <section>
       {/* Hero Section  */}
       <AboutUsHeroSection />
-      <TransformBusiness />
-      <Ambition />
+      <div className=" flex flex-col gap-[100px]">
+        <TransformBusiness />
+        <Ambition />
+        <AboutFounder />
+      </div>
     </section>
   );
 };
