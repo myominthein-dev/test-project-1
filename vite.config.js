b6beb70ce2c61825a1e056fef240abcb1c3a4f6f
Vee-Scored <myomin9916@gmail.com>
2024-11-08 14:05:14 +0630

    notFound page added

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base : "/",
   plugins: [react()],
   build: {
     rollupOptions: {
       external: ['aos', 'framer-motion', '@jdion/tilt-react']
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
     }
   },
   optimizeDeps: {
     include: ['framer-motion', 'aos', '@jdion/tilt-react']
-  },
-
-  build: {
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
   }
 });
