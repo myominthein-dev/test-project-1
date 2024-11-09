4739f1626743689075d593ce3331685cbc0c679c
Zarmani <dthar06@gmail.com>
2024-11-09 21:07:59 +0700

    some touches

--- src/components/DesignCard.jsx
+++ src/components/DesignCard.jsx
@@ -1,11 +1,11 @@
-import { Tilt } from "@jdion/tilt-react";
-import { HiArrowRight } from "react-icons/hi";
+import { Tilt } from '@jdion/tilt-react';
+import { HiArrowRight } from 'react-icons/hi';
 
 const DesignCard = ({ img, tag, date, headline, paragraph }) => {
   return (
-    <div className="flex flex-col w-[352px] md:w-[370px] lg:w-[340px] xl:w-[404px] mx-auto rounded-[20px] shadow-xl">
+    <div className="flex flex-col w-[340px] md:w-[370px] lg:w-[340px] xl:w-[404px] mx-auto rounded-[20px] shadow-xl rounded-lg">
       <div className="w-full ">
-        <img src={img} className="w-2/3 mx-auto" alt="" />
+        <img src={img} className="w-2/4 md:w-2/3 mx-auto" alt="" />
       </div>
       <div className="py-6 px-8 bg-lightBlue">
         <div className="flex gap-4 items-center">
@@ -14,12 +14,12 @@ const DesignCard = ({ img, tag, date, headline, paragraph }) => {
           </span>
           <span className="text-secondaryGrey font-lato text-base">{date}</span>
         </div>
-        <h4 className="text-black-PRIMARY text-[25px] font-futura font-medium my-3">
+        <h4 className="text-black-PRIMARY text-[23px] md:text-[25px] font-futura font-medium my-2 md:my-3">
           {headline}
         </h4>
-        <p className="font-lato text-secondaryGrey text-balance">{paragraph}</p>
+        <p className="font-lato text-secondaryGrey text-base">{paragraph}</p>
         <button className="flex group gap-3 items-center mt-4 text-blue-PRIMARY border-b duration-150 hover:border-b-blue-PRIMARY">
-          Link <HiArrowRight className="group-hover:-rotate-45 duration-150" />{" "}
+          Link <HiArrowRight className="group-hover:-rotate-45 duration-150" />{' '}
         </button>
       </div>
     </div>
