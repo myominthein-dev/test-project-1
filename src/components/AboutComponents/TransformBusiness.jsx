9431988efe18007a7fed22cb19f97798f1208d1f
blankverse123000 <blankverse123000@gmail.com>
2024-11-09 21:58:07 +0800

    added_animation_to_TramsformingPage

--- src/components/AboutComponents/TransformBusiness.jsx
+++ src/components/AboutComponents/TransformBusiness.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import Container from "../Container";
 import desktopComputer from "../../assets/AboutAssets/desktop-computer.png";
 import Button from "./Button";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const TransformBusiness = () => {
+  Aos.init();
   return (
     <section className="">
       <Container className={"py-5 px-5 flex justify-center"}>
         <div className=" flex md:flex-row flex-col gap-[10px] lg:w-[1220px] md:w-[764px] w-[352px] md:gap-[20px]  justify-between  text-left items-center ">
-          <div className="flex flex-col  gap-2.5 lg:w-[491px] md:w-[372px] w-[352px] ">
+          <div
+            data-aos="flip-up"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="cubic-bezier(0.1, -0.6, 0.2, 0)"
+            data-aos-mirror="true"
+            data-aos-anchor-placement="top-center"
+            className="flex flex-col  gap-2.5 lg:w-[491px] md:w-[372px] w-[352px] "
+          >
             <h1 className=" font-futura font-bold text-2xl   md:text-[22px] text-[20px] lg:text-[31px]  my-1 leading-[41.2px] sm:tracking-wide">
               Transforming <br className="" /> business since 2014
             </h1>
@@ -28,7 +39,16 @@ const TransformBusiness = () => {
             <Button className={"mt-5 items-end"} label={"Learn More"} />
           </div>
 
-          <div className=" md:flex items-center justify-center md:mt-0 mt-5  hidden ">
+          <div
+            data-aos="flip-right"
+            // data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="cubic-bezier(0.1, -0.6, 0.2, 0)"
+            data-aos-mirror="true"
+            data-aos-anchor-placement="top-center"
+            className=" md:flex items-center justify-center md:mt-0 mt-5  hidden "
+          >
             <img
               className="lg:w-[531px] md:w-[372px] w-[352px]"
               src={desktopComputer}
